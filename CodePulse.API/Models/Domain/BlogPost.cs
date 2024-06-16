308c600d024f0c20b4e73c6884196b27371cca10
mangala-karunarathne <mangalaicc@gmail.comm>
2024-06-17 00:15:41 +0530

    Add project files.

---
+++ CodePulse.API/Models/Domain/BlogPost.cs
@@ -0,0 +1,15 @@
+﻿namespace CodePulse.API.Models.Domain
+{
+    public class BlogPost
+    {
+        public Guid Id { get; set; }
+        public String Title { get; set; }
+        public String ShortDescription { get; set; }
+        public String Content { get; set; }
+        public String FeatureImageUrl { get; set; }
+        public String UrlHandle { get; set; }
+        public DateTime PublishedDate { get; set; }
+        public String Author { get; set; }
+        public bool IsVisible { get; set; }
+    }
+}
