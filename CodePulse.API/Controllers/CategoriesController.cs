079016c6533464c827ffa4430227ed75e4a7ab90
mangala-karunarathne <mangalaicc@gmail.comm>
2024-06-28 22:14:31 +0530

    CHG | Create API for Categories

---
+++ CodePulse.API/Controllers/CategoriesController.cs
@@ -0,0 +1,47 @@
+﻿using CodePulse.API.Data;
+using CodePulse.API.Models.Domain;
+using CodePulse.API.Models.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodePulse.API.Controllers
+{
+    // https://localhost:xxx/api/categories
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        // Add to address the dbContext unavailabe in controller isssue
+        private ApplicationDbContext dbContext;
+
+        public CategoriesController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateCategoryRequestDto request)
+        {
+            // Map DTO to Domain Model
+            var category = new Category
+            {
+                Name = request.Name,
+                UrlHandle = request.UrlHandle,
+            };
+
+            await dbContext.Categories.AddAsync(category);
+            await dbContext.SaveChangesAsync();
+
+            // Map Model to DTO
+            var response = new CategoryDto
+            {
+                Id = category.Id,
+                Name = category.Name,
+                UrlHandle = category.UrlHandle,
+            };
+
+            return Ok(category);
+        }
+    }
+}
---
+++ CodePulse.API/Models/DTO/CreateCategoryRequestDto.cs
@@ -0,0 +1,8 @@
+﻿namespace CodePulse.API.Models.DTO
+{
+    public class CreateCategoryRequestDto
+    {
+        public String Name { get; set; }
+        public String UrlHandle { get; set; }
+    }
+}
